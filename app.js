05c426324e85113193e604532121c336a31bd033
RohitPatilRRP <rohit.patil.rrp16@gmail.com>
2019-08-01 01:06:46 +0530

    ol

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ const port = process.env.PORT || 3000;
 
 const uri = 'mongodb+srv://root:123@cluster0-razzt.mongodb.net/test';
 
-const db = mongoose.connect(uri,{useNewUrlParser: true}).then(()=>{ console.log('connected')}).catch((error) => { console.log(error); });
+const db = mongoose.connect(uri, { useNewUrlParser: true }).then(() => { console.log('connected') }).catch((error) => { console.log(error); });
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser());
 app.use(fileUpload());
 
-app.use('/',require('./routes/index'));
-app.use('/users',require('./routes/users'));
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 app.listen(port, () => {
     console.log(`listning on ${port}`);
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,42 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const multer = require('multer');
+const path = require('path');
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+    destination: './routes/uploads/',
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+// Init Upload
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1000000 },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('img');
+
+
+function checkFileType(file, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
+
 
 
 //login
@@ -22,7 +58,7 @@ router.post('/register', (req, res) => {
     if (password.length < 6) {
         errors.push({ msg: 'Password must be at least 6 characters' });
     }
-    if (req.files.img.size == 0 ) {
+    if (req.files.img.size == 0) {
         return res.status(400).send('No files were uploaded.');
     }
 
@@ -31,21 +67,42 @@ router.post('/register', (req, res) => {
     }
 
     else {
-        const image =req.files.img;
+        let file;
         User.findOne({ email: email }).then((user) => {
             if (user) {
                 res.status(500).send('user exits');
             }
             else {
-                let sampleFile = req.files.img;
-                sampleFile.mv(__dirname + '/uploads/'+ email , function (err) {
+
+               
+                upload(req, res, (err) => {
                     if (err) {
-                        return res.status(500).send(err);
-                    }
-                    else {
-                        console.log('image uploaded');
+                        res.json({
+                            msg: err
+                        });
+                    } else {
+                        console.log('this',req.file);
+                        if (req.file == undefined) {
+                            res.json({
+                                msg: 'Error: No File Selected!'
+                            });
+                        } else {
+
+                            console.log('File Uploaded!');
+                            file = `uploads/${req.file.filename}`;
+
+                        }
                     }
                 });
+                // let sampleFile = req.files.img;
+                // sampleFile.mv(__dirname + '/uploads/'+ email , function (err) {
+                //     if (err) {
+                //         return res.status(500).send(err);
+                //     }
+                //     else {
+                //         console.log('image uploaded');
+                //     }
+                // });
 
 
                 const newUser = new User({
@@ -53,25 +110,25 @@ router.post('/register', (req, res) => {
                     lastname,
                     email,
                     password,
-                    image
+                    file
                 });
 
-         //       console.log('this ',newUser);
+                //       console.log('this ',newUser);
 
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
-                      if (err) throw err;
-                      newUser.password = hash;
-                      newUser
-                        .save()
-                        .then(user => {
-                          res.json({
-                            message: 'success_msg'
-                          });
-                        })
-                        .catch(err => console.log(err));
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser
+                            .save()
+                            .then(user => {
+                                res.json({
+                                    message: 'success_msg'
+                                });
+                            })
+                            .catch(err => console.log(err));
                     });
-                  });
+                });
             }
         })
 
